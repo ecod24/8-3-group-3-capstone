9fe8e8fe5fa6f9c5ceb25f9ffcd316aa69d7a1d6
Abdullah-Tasfiek <atasfiek.ece@gmail.com>
2022-11-16 12:37:47 -0500

    added foods table

--- backend/db/schema.sql
+++ backend/db/schema.sql
@@ -6,4 +6,10 @@ CREATE TABLE users (
     id SERIAL PRIMARY KEY,
     name TEXT NOT NULL,
     image TEXT NOT NULL
-)
+);
+
+CREATE TABLE foods (
+    id SERIAL PRIMARY KEY,
+    name TEXT NOT NULL,
+    image TEXT NOT NULL
+);
--- backend/db/seed.sql
+++ backend/db/seed.sql
@@ -5,3 +5,9 @@ INSERT INTO users (name, image) VALUES
 ('Abdullah Tasfiek', 'blank image'),
 ('Hermanclyde Guerrero', 'blank image'),
 ('Charlie Moran', 'blank image');
+
+INSERT INTO foods (name, image) VALUES
+('Birria Tacos', 'https://www.swankyrecipes.com/wp-content/uploads/2022/08/Birria-Tacos.jpg'),
+('Korean Fried Chicken', 'https://www.cookinwithmima.com/wp-content/uploads/2022/06/gochujang-chicken-tenders.jpg'),
+('Ramen', 'https://www.halfbakedharvest.com/wp-content/uploads/2021/10/Easy-Ginger-Chicken-and-Spinach-Ramen-1.jpg'),
+('Shawarma', 'https://www.foxyfolksy.com/wp-content/uploads/2021/09/chicken-shawarma.jpg')
