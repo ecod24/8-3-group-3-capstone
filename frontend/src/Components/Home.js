922bfd25ddc6bc779c07aa851c68c0a820c043c5
H.C.Guerrero <clydeguerrero1@gmail.com>
2022-11-15 22:13:47 -0500

    Added Changes to Home.js, now populates with backend data

--- frontend/src/Components/Home.js
+++ frontend/src/Components/Home.js
@@ -1,11 +1,29 @@
+import axios from 'axios';
 import { Link } from 'react-router-dom';
+import { useState, useEffect } from 'react';
+import User from './User';
 
 export default function Home() {
+  const URL = process.env.REACT_APP_API_URL;
+  const [users, getAllUsers] = useState([]);
+  useEffect(() => {
+    axios
+      .get(`${URL}/users`)
+      .then((res) => getAllUsers(res.data.payload))
+      .catch((err) => console.log(err.message.payload));
+  }, [URL]);
   return (
     <div className='LandingDiv'>
       <Link className='LandingLink' to='/'>
         <h1>Welcome to Table for Two!</h1>
       </Link>
+      <table>
+        <tbody>
+          {users.map((user) => {
+            return <User key={user.name} info={user} />;
+          })}
+        </tbody>
+      </table>
     </div>
   );
 }
