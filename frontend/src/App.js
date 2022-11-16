4dee5075ca4abb4e072f9b5eff0bab10992cf75b
Edwin Codrington <edwincodrington@gmail.com>
2022-11-16 12:43:05 -0500

    test to see if branch protection explodes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,18 +1,19 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import HomePage from './Pages/home.js';
-import Nav from './Pages/navbar.js';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import HomePage from "./Pages/home.js";
+import Nav from "./Pages/navbar.js";
 
-import './App.css';
+import "./App.css";
 
 function App() {
-  return (
-    <BrowserRouter>
-      <Nav />
-      <main>
-        <HomePage />
-      </main>
-    </BrowserRouter>
-  );
+	return (
+		<BrowserRouter>
+			<Nav />
+			<main>
+				<HomePage />
+			</main>
+			<></>
+		</BrowserRouter>
+	);
 }
 
 export default App;
