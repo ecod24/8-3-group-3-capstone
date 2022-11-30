894881bcb24a309204cf5540a3e72b9ec3e20617
Charlie Moran <Carlosamoran18@gmail.com>
2022-11-30 16:50:38 -0500

    User Profile upload 1

---
+++ frontend/src/Pages/UserProfile.js
@@ -0,0 +1,36 @@
+import UserProfile from '../Components/UserProfile.js';
+import { useState } from 'react';
+import Group from './Group';
+import ColorBox from './ColorBox';
+import { months, calcButtonTextColor } from '../Tools';
+
+export default function EditableUserProfile({
+    stored,
+    startEditCallback
+}) {
+
+    console.log()
+
+    const buttonStyle = {
+        backgroundColor: stored.color,
+        color: calcButtonTextColor(stored.color)
+    };
+
+    return <div>
+        <Group>
+            <h2>Name:</h2> {stored.name}
+        </Group>
+        <Group>
+            <h2>Birthday:</h2> {months.getShortName(stored.month)} {stored.day}
+        </Group>
+        <Group>
+            <h2>Favourite Color:</h2> <ColorBox color={stored.color}/>
+        </Group>
+        <Group>
+            <button
+                style={buttonStyle}
+                onClick={startEditCallback}
+            >Edit</button>
+        </Group>
+    </div>
+}
