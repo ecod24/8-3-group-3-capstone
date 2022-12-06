21ffbaaee558f89ac5fbadb19ae1e8cb94f79da9
Charlie Moran <Carlosamoran18@gmail.com>
2022-12-06 10:05:43 -0500

    updating

--- frontend/src/Components/NavBar.js
+++ frontend/src/Components/NavBar.js
@@ -1,7 +1,6 @@
 import { Link } from "react-router-dom";
 import "../Styles/NavBar.css";
 
-
 export default function NavBar() {
     const menu = document.querySelector(".menu");
     const closeIcon= document.querySelector(".closeIcon");
@@ -33,7 +32,7 @@ export default function NavBar() {
     <Link to={`/users`}><li><a class="burger" href="#">Profile</a></li></Link>
     <Link to={'/'}><li><a class="burger" href="#">Register</a></li></Link>
     <Link to={`/users/new`}><li><a class="burger" href="#">Sign In</a></li></Link>
-    <Link><li><a class="burger" href="#">Contact us</a></li></Link>
+    <Link to={`/about`}><li><a class="burger" href="#">Contact us</a></li></Link>
     </ul>
     <button class="hamburger" onClick={toggleMenu}>
     <i class="menuIcon"><img
