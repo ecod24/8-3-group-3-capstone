16daf653c996c55e8179c4c7d72a884eabe4db37
Abdullah-Tasfiek <atasfiek.ece@gmail.com>
2022-12-07 11:34:33 -0500

    react toastify, boilerplate for cloudbase chat system

--- frontend/src/Firebase.js
+++ frontend/src/Firebase.js
@@ -1,5 +1,11 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
+import {
+  getFirestore,
+  // getMessaging,
+  // collection,
+  // getDocs,
+} from "firebase/firestore/lite";
 // import { getAnalytics } from "firebase/analytics";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
@@ -18,6 +24,15 @@ const firebaseConfig = {
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
+// const messaging = getMessaging(app);
 // const analytics = getAnalytics(app);
+
+// async function getMessages(db) {
+//   const messagesCol = collection(db, "messages");
+//   const messagesSnapshot = await getDocs(messagesCol);
+//   const messageList = messagesSnapshot.docs.map((doc) => doc.data());
+//   return messageList;
+// }
 
 export default app;
