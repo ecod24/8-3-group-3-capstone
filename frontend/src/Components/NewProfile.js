31502fbbbeb1ac6f854912d4cf1f73ef0a920ad6
Edwin Codrington <edwincodrington@gmail.com>
2022-12-07 11:41:24 -0500

    another user profile change, another 5 files to refactor...

--- frontend/src/Components/NewProfile.js
+++ frontend/src/Components/NewProfile.js
@@ -17,6 +17,7 @@ export default function NewProfile() {
     sexual_orientation: '',
     gender: '',
     about_me: '',
+    chat_handle: '',
   });
 
   const newUser = (profile) => {
@@ -128,6 +129,15 @@ export default function NewProfile() {
             value={profile.about_me}
             onChange={handleTextChange}
             placeholder='Tell Us About Yourself.'
+          />
+          <label htmlFor='chat_handle'>Chat Handle:</label>
+          <input 
+            id='chat_handle'
+            className='NewUserForm'
+            type='text'
+            value={profile.chat_handle}
+            onChange={handleTextChange}
+            placeholder='Add your handle from a chatting service like Messenger, WhatsApp, Kik... etc'
           />
           <div id='buttonDiv'>
             <input type='submit' id='submit' value='Submit' />
