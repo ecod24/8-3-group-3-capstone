cc5a57c25131f87b3f9ed5ff2bc9fecd995769b0
Abdullah-Tasfiek <atasfiek.ece@gmail.com>
2022-12-09 12:03:54 -0500

    like profiles for front-end

--- frontend/src/AuthContext.js
+++ frontend/src/AuthContext.js
@@ -7,14 +7,14 @@ export const context = React.createContext();
 const API = process.env.REACT_APP_API_URL;
 
 export default function AuthProvider({ children }) {
-	const [user, setUser] = useState(null);
-	useEffect(() => {
-		auth.onAuthStateChanged((test) => {
-			axios.get(`${API}/users/firebase/${test.uid}`).then((response) => {
-				setUser(response.data.payload);
-			});
-		});
-		// User is signed in.
-	});
-	return <context.Provider value={user}>{children}</context.Provider>;
+  const [user, setUser] = useState(null);
+  useEffect(() => {
+    auth.onAuthStateChanged((test) => {
+      axios.get(`${API}/users/firebase/${test.uid}`).then((response) => {
+        setUser(response.data.payload);
+      });
+    });
+    // User is signed in.
+  });
+  return <context.Provider value={user}>{children}</context.Provider>;
 }
